c1238e3c270feaaa1476b19859c3f41de457895d
Olin Kirkland <me@olinkirk.land>
2022-09-11 01:11:30 +0200

    More styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,69 @@
 export default function Home() {
   return (
     <article>
-      <section id="hero">
-        <h1 className="logo">Angelika Paints</h1>
+      <div className="hero-background">
+        <section id="hero">
+          <h1 className="logo">Angelika Paints</h1>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo vitae
-          hic quia vel, cumque eligendi nostrum eaque doloremque aliquid
-          dolorum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Voluptatem, officia.
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo vitae
+            hic quia vel, cumque eligendi nostrum eaque doloremque aliquid
+            dolorum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatem, officia.
+          </p>
 
-        <div className="button-bar">
-          {/* <button>Lessons</button> */}
-          <button>Explore the collections</button>
-          <button className="cta">Order a Commission</button>
-        </div>
-      </section>
+          <div className="button-bar">
+            {/* <button>Lessons</button> */}
+            <button>Explore the collections</button>
+            <button className="cta">Order a Commission</button>
+          </div>
+        </section>
+      </div>
       {/* <section>About</section>
       <section>Gallery</section>
       <section>Services</section>
       <section>News</section>
       <section>Contact</section> */}
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime,
+          officiis et laboriosam culpa quis quibusdam ipsa fugit ipsam ipsum
+          deserunt!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+          reiciendis quod ipsam recusandae animi ipsum adipisci iusto veniam ad
+          repudiandae! Ullam numquam beatae consectetur eveniet!
+        </p>
+      </section>
+      <section>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident
+          incidunt fugit et fugiat vitae ratione cupiditate, in blanditiis
+          laborum itaque pariatur necessitatibus perferendis natus mollitia!
+          Nemo, quae impedit? Corrupti cupiditate similique reprehenderit nobis
+          amet? Temporibus, perspiciatis. At, modi nulla, minima ea asperiores
+          necessitatibus reiciendis nisi consequuntur nobis sint blanditiis
+          natus?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Reprehenderit non reiciendis earum ad sit velit itaque animi illo ipsa
+          ullam?
+        </p>
+      </section>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui harum
+          incidunt voluptas perferendis excepturi id optio nulla, adipisci iusto
+          molestias, cum vel accusantium mollitia eaque officia quaerat.
+          Repudiandae, expedita doloribus?
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt,
+          nisi.
+        </p>
+      </section>
     </article>
   );
 }
--- src/components/NewsletterForm.jsx
+++ src/components/NewsletterForm.jsx
@@ -1,7 +1,8 @@
 export default function NewsletterForm() {
   return (
     <div className="newsletter-prompt">
-      <h2>Sign up for our newsletter</h2>
+      <h2>Sign up for the free Angelika Paints newsletter.</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui harum.</p>
       <form>
         <input type="text" placeholder="Your email address" />
         <button type="submit">Sign up</button>
